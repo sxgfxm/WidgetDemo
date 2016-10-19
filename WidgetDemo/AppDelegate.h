c6e47900c4392aa5cc64bc6246f62d90ec2683f1
Song Xiaoguang <xgsong@mobvoi.com>
2016-10-19 15:34:32 +0800

    Initial Commit

---
+++ WidgetDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WidgetDemo
+//
+//  Created by 宋晓光 on 19/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WidgetDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WidgetDemo
+//
+//  Created by 宋晓光 on 19/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
